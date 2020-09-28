e7a05848205ff38509b19ca65eeed7a7a32ac0a9
gwinbleid <corsar909@gmail.com>
2020-09-28 05:58:08 +0300

    feat: solve extended-repeater problem

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  function addition_construct(addition, additionRepeatTimes, additionSeparator) {
+    if ('addition' in options !== true) return '';
+    if (isNaN(+additionRepeatTimes)) return '' + addition;
+    if ('additionSeparator' in options !== true) return new Array(+additionRepeatTimes).fill('' + addition).join('|');
+    return new Array(+additionRepeatTimes).fill('' + addition).join('' + additionSeparator);
+  }
+
+  function string_construct(str, repeatTimes, separator) {
+    let addition = addition_construct(options.addition, options.additionRepeatTimes, options.additionSeparator);
+
+    if (isNaN(+repeatTimes)) return str.toString() + addition;
+    if ('separator' in options !== true) {
+      return new Array(+repeatTimes).fill('' + str + addition).join('+');
+    } else {
+      return new Array(+repeatTimes).fill('' + str + addition).join('' + separator);
+    }
+  }
+
+  return string_construct(str, options.repeatTimes, options.separator);
 };
-  
