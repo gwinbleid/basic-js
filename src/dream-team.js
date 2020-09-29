5fc678d1baba1483e0a950b1ee98fc26fdb4aa45
gwinbleid <corsar909@gmail.com>
2020-09-29 12:35:21 +0300

    feat: solve dream-team problem

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam(members) {
+  if(!(members instanceof Array)) return false;
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return members
+      .filter(item => typeof item === 'string')
+      .map(item => {
+        return item.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '').slice(0, 1);
+      })
+      .map(item => item.toUpperCase())
+      .sort(function (a, b) {
+        return a.charCodeAt(0) - b.charCodeAt(0);
+      })
+      .join('');
 };
